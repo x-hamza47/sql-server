b1d4fe3e94c7ca1728fafa2dc67cf34c34d651f9
Hamza <103899474+x-hamza47@users.noreply.github.com>
2025-07-31 01:12:33 +0500

    Update sub-queries.sql

--- sub-queries.sql
+++ sub-queries.sql
@@ -6,11 +6,24 @@ CREATE TABLE students (
     phone_number VARCHAR(20),
     department_id INT
 );
+
+INSERT INTO students (stu_name, age, phone_number, department_id) VALUES
+('Ali', 22, '03001234567', 1),
+('Sara', 19, '03119876543', 2),
+('Ahmed', 24, '03219876543', 1),
+('Hina', 18, '03019873210', 3),
+('Usman', 25, '03331231231', 1),
+('Ayesha', 23, '03007894561', 2),
+('Zain', 26, '03451231231', 1);
+
 --Department Table
 CREATE TABLE departments (
     department_id INT PRIMARY KEY IDENTITY(1,1),
     department_name VARCHAR(100) NOT NULL
 );
+
+INSERT INTO departments (department_name) VALUES 
+('Computer Science'), ('Electrical Engineering'), ('Business');
 --1) Students older than average
 SELECT stu_name FROM students 
 WHERE age > (SELECT AVG(age) FROM students);
