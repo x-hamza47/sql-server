11f93f328bfae342583933125588d3ffef1b2f44
Hamza <103899474+x-hamza47@users.noreply.github.com>
2025-08-08 08:59:46 +0500

    Create taask-1.cs

---
+++ taask-1.cs
@@ -0,0 +1,14 @@
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Even numbers from 1 to 100:");
+        for (int i = 1; i <= 100; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
